7f066151cca4ab365b7e8ae51966396e543eda5c
Benny Lach <benny@discollect.me>
2017-01-10 23:34:29 +0100

    added keyboard enum

---
+++ src/ui/graphical/keyboard/KeyboardEnum.java
@@ -0,0 +1,17 @@
+package ui.graphical.keyboard;
+
+/**
+ * Enum to represent the action to perform if corresponding keyboard shortcut was triggered
+ * @author Benny Lach
+ *
+ */
+public enum KeyboardEnum {
+	New,
+	NewFromFile,
+	Save,
+	SaveTo,
+	
+	AddNode,
+	DeleteNode,
+	DeleteAll,
+}
