3ff49a0f1ec2a1a220a31796f05b14d1efd590a2
Benny Lach <benny@discollect.me>
2017-02-14 13:09:46 +0100

    Added StringValidator tests

--- src/tests/StringValidatorTests.java
+++ src/tests/StringValidatorTests.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.util.Scanner;
 
 import org.junit.Test;
 import ui.console.validation.*;
@@ -12,25 +13,25 @@ public class StringValidatorTests {
 	
 	@Test
 	public void test_nullStringValidation() {
-		StringValidationType type = StringValidator.validateString(null, null, null);
+		StringValidationType type = StringValidator.validateString(null, null, null, null);
 		assertEquals("Validation is not wrong", type, StringValidationType.Wrong);
 	}
 	
 	@Test
 	public void test_emptyStringValidation() {
-		StringValidationType type = StringValidator.validateString("", null, null);
+		StringValidationType type = StringValidator.validateString(null, "", null, null);
 		assertEquals("Validation is not wrong", type, StringValidationType.Wrong);
 	}
 	
 	@Test
 	public void test_validStringValidation() {
-		StringValidationType type = StringValidator.validateString("asf", null, null);
+		StringValidationType type = StringValidator.validateString(null, "asf", null, null);
 		assertEquals("Validation is not correct", type, StringValidationType.Correct);
 	}
 	
 	@Test
 	public void test_stringToLongNoShringValidation() {
-		StringValidationType type = StringValidator.validateString("tolong", null, false);
+		StringValidationType type = StringValidator.validateString(null, "tolong", null, false);
 		assertEquals("Validation is not CancelSequence", type, StringValidationType.CancelSequence);
 	}
 	
@@ -39,26 +40,29 @@ public class StringValidatorTests {
 		InputStream original_in = System.in;
 		
 		// Simulate user input - yes input 
-		ByteArrayInputStream in = new ByteArrayInputStream("y\nn\nfoo\n".getBytes());
-		System.setIn(in);
+		ByteArrayInputStream in = new ByteArrayInputStream("y\r\n".getBytes());
 		
-		StringValidationType type = StringValidator.validateString("tolong", null, true);
-		assertEquals("Validation is not ShrinkInput", type, StringValidationType.ShrinkInput);
+		StringValidationType type = StringValidator.validateString(new Scanner(in), "tolong", null, true);
+		assertEquals("Validation is not ShrinkInput", StringValidationType.ShrinkInput, type);
 		
 		// Simulate user input - no parameter 
-		type = StringValidator.validateString("tolong", null, true);
-		System.out.println("\n" + type + "\n");
-		assertEquals("Validation is not CancelSequence", type, StringValidationType.CancelSequence);
+		in = new ByteArrayInputStream("n\r\n".getBytes());
 		
-		// Simulate user input - wrong parameter 
-		type = StringValidator.validateString("tolong", null, true);
-		assertEquals("Validation is not CancelSequence", type, StringValidationType.CancelSequence);
+		type = StringValidator.validateString(new Scanner(in), "tolong", null, true);
+		assertEquals("Validation is not CancelSequence", StringValidationType.CancelSequence, type);
+		
+		// Simulate user input - wrong parameter
+		in = new ByteArrayInputStream("foo\r\n".getBytes());
+		
+		type = StringValidator.validateString(new Scanner(in), "tolong", null, true);
+		assertEquals("Validation is not CancelSequence", StringValidationType.CancelSequence, type);
+		
 		System.setIn(original_in);
 	}
 	
 	@Test
 	public void test_cancelSequenceTest() {
-		StringValidationType type = StringValidator.validateString("cancel", "cancel", false);
-		assertEquals("Validation is not CancelSequence", type, StringValidationType.CancelSequence);
+		StringValidationType type = StringValidator.validateString(null, "cancel", "cancel", false);
+		assertEquals("Validation is not CancelSequence", StringValidationType.CancelSequence, type);
 	}
 }
