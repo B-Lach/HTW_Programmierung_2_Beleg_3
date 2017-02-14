2349e32cf7d918987189409697b2e77d159153a1
Benny Lach <benny@discollect.me>
2017-02-14 14:40:33 +0100

    Added two test cases in Main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+import java.io.File;
 import java.util.Scanner;
 
+import logic.BinaryTree;
 import ui.*;
+import ui.console.input.InputHandler;
+
 /**
  * Entry Point of the program
  * @author Benny Lach
@@ -8,10 +12,12 @@ import ui.*;
  */
 public class Main {
 
-	private static Scanner scanner;
+	private final static Scanner scanner = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		
+		// only used for testing 
+//		testBinaryTreeAddition();
+//		testBinaryTreeDeletion();
 		UIType outputType = getProgramType();
 		
 		if(outputType != null) {
@@ -22,12 +28,16 @@ public class Main {
 		}
 	}
 	
+	/**
+	 * Function to get the preferred output type selected by the user
+	 * @return The UIType the user wants to use. Is null if the made input was not correct. 
+	 */
 	private static UIType getProgramType() {
 		System.out.println("Which program type do you want to use?");
 		System.out.println("Press <1> for console output");
 		System.out.println("Press <2> for window output");
 		
-		int option = getIntInput();
+		int option = InputHandler.getIntInput(scanner);
 		
 		switch(option) {
 		case 1:
@@ -39,16 +49,46 @@ public class Main {
 		}
 	}
 	
-	private static int getIntInput() {
-		scanner = new Scanner(System.in);
-		int option = -1;
-		// Workaround for scanner.nextInt()
-		// See: http://stackoverflow.com/questions/13102045/scanner-is-skipping-nextline-after-using-next-nextint-or-other-nextfoo
-		try {
-			option = Integer.parseInt(scanner.nextLine());
-		} catch(Exception e) {
+	/**
+	 * Function to test addition
+	 */
+	private static void testBinaryTreeAddition() {
+		File valid = new File("src/tests/resources/addition.btv");
+		
+		BinaryTree tree = BinaryTree.loadTreeFromFile(valid.getAbsolutePath());
+		if (tree != null) {
+			System.out.println("Loadad Tree:");
+			tree.printTree();
+			
+			tree.addNode("06");
+			tree.addNode("41");
+			tree.addNode("55");
+			tree.addNode("80");
+			
+			System.out.println("Tree after addition of 06, 41, 55, 80");
+			tree.printTree();
 			
 		}
-		return option;
+	}
+	
+	/**
+	 * Function to test deletion
+	 */
+	private static void testBinaryTreeDeletion() {
+		File valid = new File("src/tests/resources/deletion.btv");
+		
+		BinaryTree tree = BinaryTree.loadTreeFromFile(valid.getAbsolutePath());
+		
+		if (tree != null) {
+			System.out.println("Loadad Tree:");
+			tree.printTree();
+			
+			tree.deleteNode("30");
+			tree.deleteNode("60");
+			
+			System.out.println("Tree after deletion of 30, 60");
+			tree.printTree();
+			
+		}
 	}
 }
